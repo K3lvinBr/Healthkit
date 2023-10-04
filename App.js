ed6b4eb161c4294684cf36159d05771b63265311
Kelvin <kelvinsalesmachado@gmail.com>
2023-10-04 18:26:34 -0300

    04/10

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+import AppleHealthKit from 'react-native-health'
 import { useEffect, useState } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-import AppleHealthKit, {
-  HealthValue,
-  HealthKitPermissions,
-} from 'react-native-health'
 
 const permissions = {
   permissions: {
@@ -18,6 +14,8 @@ export default function App() {
 
   const [hasPermissions, setHasPermissions] = useState(false)
   const [steps, setSteps] = useState(0)
+  const [heart, setHeart] = useState([])
+  const [sleep, setSleep] = useState([])
 
   useEffect(() => {
     if (Platform.OS === 'ios') {
@@ -52,21 +50,54 @@ export default function App() {
       },
     )
 
+    AppleHealthKit.getHeartRateSamples(
+      options,
+      (error, results) => {
+        if (error) {
+          console.log('Error getting the heart samples')
+        }
+        console.log(results.value)
+        setHeart(results.value[0].value)
+      },
+    )
+
+    AppleHealthKit.getSleepSamples(
+      options,
+      (error, results) => {
+        if (error) {
+          console.log('Error getting the sleep samples')
+        }
+        console.log(results.value)
+
+        function getMinutesBetweenDates(startDate, endDate) {
+          var diff = endDate.getTime() - startDate.getTime();
+          return (diff / 60000);
+        }
+
+        const start = Date.parse(results.value[0].startDate);
+        const end = Date.parse(results.value[0].endDate);
+
+        setHeart(getMinutesBetweenDates(start, end))
+      },
+    )
+
+
+
   }, [hasPermissions])
 
   return (
-    <View style={styles.container}>
-      <Text>{steps.toString()}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar translucent hidden backgroundColor='transparent' />
+      <WebView
+        style={styles.container}
+        source={{ uri: 'https://app.umanu.com.br/' }}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
